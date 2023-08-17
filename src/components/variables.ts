da70c6d2d1a413ee0d0c7e419f8b918efe4ec814
JennyShih <gh87783gh@gmail.com>
2023-08-17 21:40:05 +0800

    clean code

--- src/components/variables.ts
+++ src/components/variables.ts
@@ -5,37 +5,10 @@ export const colors = {
   light: "#fff",
   lightDarker: "#8A8A8F",
 };
-
 export const fontSizeBase = 16;
-
-export const gradient = {
-  slider: "linear-gradient(180deg, #004D5E 0%, #007691 100%);",
-};
-
-const size = {
-  tablet: 576,
-  screen: 768,
-};
-export const device = {
-  mobile: `(max-width: ${size.tablet - 1}px)`,
-  tablet: `(min-width: ${size.tablet}px)`,
-  screen: `(min-width: ${size.screen}px)`,
-};
-/* 
-  how to use:
-  @media ${device.mobile} {
-    max-width: 200px;
-  }
-*/
-
-export const gap = {
-  normal: "16px",
-};
-
 export const layout = {
   sideBarWidth: "80px",
 };
-
 export const mixins = {
   resetUl: "padding-left: unset; margin-bottom: unset; list-style-type: none;",
   clearFix: `
@@ -51,4 +24,27 @@ export const mixins = {
     left: 50%;
     transform: translate(-50%, -50%);
   `,
+};
+
+// TODO
+const size = {
+  tablet: 576,
+  screen: 768,
+};
+export const device = {
+  mobile: `(max-width: ${size.tablet - 1}px)`,
+  tablet: `(min-width: ${size.tablet}px)`,
+  screen: `(min-width: ${size.screen}px)`,
+};
+/* 
+  how to use:
+  @media ${device.mobile} {
+    max-width: 200px;
+  }
+*/
+export const gradient = {
+  slider: "linear-gradient(180deg, #004D5E 0%, #007691 100%);",
+};
+export const gap = {
+  normal: "16px",
 };
